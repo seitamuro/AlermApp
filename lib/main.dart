a0379d84ae10c430d777366896f5276c4ba3d1b1
seitamuro <seitamuro0210@gmail.com>
2023-03-06 21:41:14 +0900

    slidableのバージョンを下げた

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import 'alerm.dart';
