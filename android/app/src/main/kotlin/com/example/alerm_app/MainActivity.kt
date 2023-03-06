72d7b3ad5ef7b5a5134afe80ee069c3b7cc0f2f7
seitamuro <seitamuro0210@gmail.com>
2023-03-06 20:22:48 +0900

    define Alerm class

---
+++ android/app/src/main/kotlin/com/example/alerm_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alerm_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/alerm.dart
@@ -0,0 +1,7 @@
+class Alerm {
+  int id;
+  DateTime alermTime;
+  bool isActive;
+
+  Alerm({this.id = 0, required this.alermTime, this.isActive = true});
+}
